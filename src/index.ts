1aaa3dec198be34e18fd72e2925bf229e671e78a
Christophe <c.applanat@gmail.com>
2023-01-17 14:57:19 +0100

    comment index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import cors from 'cors'
 import express from 'express'
 import { config } from '~/config'
+
+/** Import customs routes */
 import { TestRoutes } from './resources/test/test.routes'
 import { UsersRoutes } from './resources/users/users.routes'
 import { WoodsRoutes } from './resources/woods/woods.routes'
@@ -9,6 +11,7 @@ const app = express()
 
 app.use(express.json())
 
+/** For URL encoded bodies (the kind produced by HTTP form POSTs) */
 app.use(express.urlencoded({
     extended: true
   }));
@@ -16,10 +19,12 @@ app.use(express.urlencoded({
 
 app.use(cors())
 
+/** Customs routes */
 app.use('/test', TestRoutes)
 app.use('/users', UsersRoutes)
 app.use('/woods', WoodsRoutes)
 
+/** Homepage */
 app.get('/', (req, res) => res.send('🏠'))
 
 app.listen(config.API_PORT, () =>  console.log('Server started !'))
